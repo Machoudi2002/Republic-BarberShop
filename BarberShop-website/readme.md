e8dbda354e8e7f0bbd5e930283560a069b62e5b4
Machoudi <78318497+Machoudi2002@users.noreply.github.com>
2023-06-01 20:31:19 +0100

    Add files via upload

---
+++ BarberShop-website/readme.md
@@ -0,0 +1,60 @@
+
+
+![Barbershop Website](Images/REPUBLIC.png)
+# Barbershop Website
+
+This is a responsive barbershop website showcasing various services offered, team members, testimonials, and a contact form. The website is built using HTML, CSS, and JavaScript, with the addition of Swipe.js for image carousel functionality and Email.js for sending emails from the contact form.
+
+## Features
+
+- Responsive design for optimal viewing on different devices.
+- Interactive image carousel using Swipe.js library.
+- Contact form with validation and email sending functionality using Email.js.
+- Integration of Font Awesome for stylish icons.
+- Utilization of Google Fonts for attractive typography.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- SwiperJS (https://swiperjs.com) - for the image slider
+- EmailJS (https://www.emailjs.com) - for sending emails from the contact form
+
+## Installation and Usage
+
+1. Clone the repository or download the ZIP file.
+2. Open the project folder in your preferred code editor.
+3. Edit the HTML, CSS, and JavaScript files according to your requirements.
+4. Replace the placeholders in the JavaScript file with your own EmailJS credentials.
+5. Open the `index.html` file in your web browser to view the website.
+
+## Folder Structure
+
+```
+├── style.css/
+├── script.js/
+├── images/
+│   ├── (place your images here)
+├── index.html
+├── README.md
+```
+
+- The `style.css` file contains the CSS stylesheets.
+- The `script.js` file contains the JavaScript for the main functionality.
+- The `images` folder is where you can place your images.
+- The `index.html` file is the main HTML file for the website.
+- The `README.md` file contains information about the project.
+
+## Credits
+
+- SwiperJS: https://swiperjs.com
+- EmailJS: https://www.emailjs.com
+- Google Fonts: https://fonts.google.com/
+- Font Awesome: https://fontawesome.com/
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
+Feel free to use and modify the code as per your needs.
---
+++ BarberShop-website/script.js
@@ -0,0 +1,144 @@
+
+//////////////////////////////// Header //////////////////////////////////////////////////////////////////
+
+const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      navbar.classList.add("show");
+      menuBtn.classList.add("hide");
+      body.classList.add("disabled");
+    }
+    cancelBtn.onclick = ()=>{
+      body.classList.remove("disabled");
+      navbar.classList.remove("show");
+      menuBtn.classList.remove("hide");
+    }
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
+
+///////////////////////////////////////////////Barbers //////////////////////////////////////////////////////////////////
+
+const barbersImg = document.querySelectorAll(".barbers-images img");
+
+const barberNames = ["LEON NICKWELL", "JAMES ALAN", "NIKOLAS FERNANDES", "LEAM MENDI"];
+
+let selectedImage = null;
+
+const changeInfo = (event) => {
+  const clickedImage = event.target;
+  const index = Array.from(barbersImg).indexOf(clickedImage);
+  console.log(index);
+
+  if (selectedImage) {
+    selectedImage.style.border = "none";
+  }
+
+  if (selectedImage !== clickedImage) {
+    clickedImage.style.border = "solid 4px #FFDE59";
+    selectedImage = clickedImage;
+
+    const insta = document.querySelector("#insta")
+    const barberName = document.querySelector("#barbername");
+    barberName.textContent = barberNames[index];
+    let instUserName = barberNames[index].replace(/\s/g, "").toLocaleLowerCase();
+    console.log(instUserName)
+    insta.textContent = "@" + instUserName;
+    
+  } else {
+    selectedImage = null;
+  }
+};
+
+for (let i = 0; i < barbersImg.length; i++) {
+  barbersImg[i].addEventListener("click", changeInfo);
+}
+
+
+//////////////////////////////// header SWIPER ////////////////////////////////////////////////////
+
+
+let swiper1 = new Swiper(".headerSwiper", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+  },
+});
+
+
+
+////////////////////////////////second swiper/////////////////////////////////////
+let swiper;
+
+function initSwiper() {
+  swiper2 = new Swiper(".secondSwiper", {
+    slidesPerView: getSlidesPerView(),
+    spaceBetween: 30,
+    centeredSlides: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
+
+function getSlidesPerView() {
+  if (window.innerWidth < 600) {
+    return 1;
+  } else if (window.innerWidth < 800) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
+
+function updateSwiperConfig() {
+  swiper2.params.slidesPerView = getSlidesPerView();
+  swiper2.update();
+}
+
+// Initialize Swiper
+initSwiper();
+
+// Listen for window resize event
+window.addEventListener("resize", () => {
+  updateSwiperConfig();
+});
+
+
+///////////////////////////////////////////Send Email ////////////////////////////////////////////////////////
+
+
+const info = document.querySelectorAll("input")
+
+
+const sendMail = () => {
+  let templateParams = {
+    fullname: info[0].value,
+    email: info[1].value,
+    date: info[2].value,
+    time: info[3].value,
+    numberofppl: info[4].value
+  };
+  
+  emailjs.send('service_5lrmewg','template_jw0ikdv', templateParams)
+  .then(function(response) {
+     console.log('SUCCESS!', response.status, response.text);
+     info[5].value = "Email Recieved"
+     setTimeout(() => {
+      info[5].value = "Book Now"
+
+    }, "4000");
+  }, function(err) {
+     console.log('FAILED...', err);
+  });
+
+}
+
+
+
